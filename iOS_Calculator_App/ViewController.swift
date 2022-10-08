66bd59303ee1ee36d4e6af5972d7c565f33cbdbd
Wilson Mungai <wilsonmungai600@gmail.com>
2022-10-08 00:08:24 -0400

    user interface

---
+++ iOS_Calculator_App/ViewController.swift
@@ -0,0 +1,56 @@
+/*
+ * File Name: Calculator Milestone One
+ * Last Modified: 25/09/2022
+ * Version1
+ *
+ * Author: Wilson Mungai
+ * Student ID: 301287641
+ *
+ * Author: Gideon Shewana
+ * Student ID: 301195054
+ *
+ * Author: Bing Pan
+ * Student ID: 301317241
+
+ * Description:
+ * This Calculator App allows users to add, subtract, multiply, divide integers as well as decimal numbers and displays the results.
+ * During the input, it gives users the options to change or clear what they already type.
+ * Specifically, the Back button allows the users to remove what are entered backwards.
+ * The AC button allows the users to clear all the input numbers at once.
+ * In contract, the CE button allows users just to remove their latest entry.
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Result Label
+    @IBOutlet weak var ResultLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    // Event Handlers
+    
+    @IBAction func OperatorButton_Pressed(_ sender: UIButton)
+    {
+       
+    }
+    
+    @IBAction func NumberButton_Pressed(_ sender: UIButton)
+    {
+    }
+    
+    @IBAction func ExtraButton_Pressed(_ sender: UIButton)
+    {
+        
+        }
+    
+    }
+
+}
+
+
+
